1a0f2b1462feaa84e0d4c00d29b56cfe18bfb9dc
AKITA-physics <125631644+AKITA-physics@users.noreply.github.com>
2023-09-26 00:13:07 +0900

    4_part_spectrum_10minute_data.py

--- 4_part_spectrum_10minute_data.py
+++ 4_part_spectrum_10minute_data.py
@@ -1,21 +1,22 @@
+# パワースペクトルを多項式でフィッティングするプログラム
 import pandas as pd
 import numpy as np
 from scipy import signal
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-# --------------------------------------------------------データのダウンロード-----------------------------------------------------------------
-df_location = pd.read_csv("C:\\Users\\AKITA KOSUKE\\Box\\1_修士課程研究\\プログラム\\地点情報\\location.csv")
+# 地点データのダウンロード
+df_location = pd.read_csv("file path")
 # 処理した地域をまとめる
 list_block = []
 
-for l in [4, 153, 154]:#range(len(df_location)):
+for l in range(len(df_location)):
     no = df_location.loc[l,"no"]
     block_no = df_location.loc[l,"block_no"]
     for year in range(2022,2022+1):
-        #データのダウンロード
+        # 気象データのダウンロード
         # 補間後のデータ
-        df_original = pd.read_csv("D:\\master_research\\地上データ\\10minute_data_surface\\interplolation\\"+str(year)+"_"+str(block_no)+"_10minute_interpolation.csv")
+        df_original = pd.read_csv("file path")
 
         # カラム'mean'の欠損値の個数
         missing_num = df_original['mean'].isnull().sum()
@@ -25,7 +26,6 @@ for l in [4, 153, 154]:#range(len(df_location)):
         # 欠損がない場合のみ処理
         if missing_rate == 0:
             #---------------------------------------------------------高速フーリエ変換----------------------------------------------------------
-            # df_dataの平均風速のデータに対して観測値から平均値を引く
             signal = np.array(df_original["mean"])
             # 線形トレンドの除去
             signal_d = signal - np.polyval(np.polyfit(np.arange(len(signal)), signal, 1), np.arange(len(signal)))
@@ -37,12 +37,12 @@ for l in [4, 153, 154]:#range(len(df_location)):
             # 周波数軸の作成
             freq = np.fft.rfftfreq(N, 1/sampling_rate)
             freq = freq[1:]
-                    
             # FFTを計算
             fft_result = np.fft.rfft(signal_d)
             fft_result = np.abs(fft_result)
             fft_result = fft_result[1:]
-            
+
+            #------------------------------------------平滑化---------------------------------------------
             # リストの中の数を平均して減らす
             def average_of_groups(lst, group_sizes):
                 averages = []
@@ -68,7 +68,7 @@ for l in [4, 153, 154]:#range(len(df_location)):
             # 移動平均フィルタを適用
             fft_result = np.convolve(fft_result, np.ones(window_size)/window_size, mode='same')
 
-            # 強度の計算
+            # ------------------------------------------------------強度の計算-----------------------------------------------------------
             spectrum = freq * fft_result ** 2 / sampling_rate /(N/2)
             exec(f"spectrum_{block_no}_{year} = spectrum")
             
@@ -100,27 +100,27 @@ for l in [4, 153, 154]:#range(len(df_location)):
             exec(f"slope_{block_no}_all = regressor.coef_[0]")
             exec(f"intercept_{block_no}_all = regressor.intercept_")
             
-            # # 低周波数領域のみを取り出す
-            # selected_freq_low = [f for f in freq if  1/18000 < f < 1/7200]
-            # selected_spectrum_low = [np.power(10, y_fit)[i] for i, f in enumerate(freq) if f in selected_freq_low]
-            # #スペクトルの最小二乗法
-            # x = np.log10(np.array(selected_freq_low).reshape(-1, 1))
-            # y = np.log10(np.array(selected_spectrum_low).reshape(-1, 1))
-            # regressor = LinearRegression().fit(x, y)
-            # # 傾きと切片の取得
-            # exec(f"slope_{block_no}_low = regressor.coef_[0]")
-            # exec(f"intercept_{block_no}_low = regressor.intercept_")
+            # 低周波数領域のみを取り出す
+            selected_freq_low = [f for f in freq if  1/18000 < f < 1/7200]
+            selected_spectrum_low = [np.power(10, y_fit)[i] for i, f in enumerate(freq) if f in selected_freq_low]
+            #スペクトルの最小二乗法
+            x = np.log10(np.array(selected_freq_low).reshape(-1, 1))
+            y = np.log10(np.array(selected_spectrum_low).reshape(-1, 1))
+            regressor = LinearRegression().fit(x, y)
+            # 傾きと切片の取得
+            exec(f"slope_{block_no}_low = regressor.coef_[0]")
+            exec(f"intercept_{block_no}_low = regressor.intercept_")
             
-            # # 高周波数領域のみを取り出す
-            # selected_freq_high = [f for f in freq if  1/14400 < f < 1/7200]
-            # selected_spectrum_high = [np.power(10, y_fit)[i] for i, f in enumerate(freq) if f in selected_freq_high]
-            # #スペクトルの最小二乗法
-            # x = np.log10(np.array(selected_freq_high).reshape(-1, 1))
-            # y = np.log10(np.array(selected_spectrum_high).reshape(-1, 1))
-            # regressor = LinearRegression().fit(x, y)
-            # # 傾きと切片の取得
-            # exec(f"slope_{block_no}_high = regressor.coef_[0]")
-            # exec(f"intercept_{block_no}_high = regressor.intercept_")
+            # 高周波数領域のみを取り出す
+            selected_freq_high = [f for f in freq if  1/14400 < f < 1/7200]
+            selected_spectrum_high = [np.power(10, y_fit)[i] for i, f in enumerate(freq) if f in selected_freq_high]
+            #スペクトルの最小二乗法
+            x = np.log10(np.array(selected_freq_high).reshape(-1, 1))
+            y = np.log10(np.array(selected_spectrum_high).reshape(-1, 1))
+            regressor = LinearRegression().fit(x, y)
+            # 傾きと切片の取得
+            exec(f"slope_{block_no}_high = regressor.coef_[0]")
+            exec(f"intercept_{block_no}_high = regressor.intercept_")
             
             # 計算に使った風速の観測地をまとめる
             list_block.append(block_no)
@@ -137,12 +137,12 @@ for i in range(len(df_summary)):
     block_no = df_summary.loc[i, "block_no"]
     df_summary.loc[i, "mean_wind"] = eval(f"mean_wind_{block_no}")
     df_summary.loc[i, "slope_all"] = eval(f"slope_{block_no}_all")
-    # df_summary.loc[i, "slope_high"] = eval(f"slope_{block_no}_high")
-    # df_summary.loc[i, "slope_low"] = eval(f"slope_{block_no}_low")
+    df_summary.loc[i, "slope_high"] = eval(f"slope_{block_no}_high")
+    df_summary.loc[i, "slope_low"] = eval(f"slope_{block_no}_low")
 
 print(df_summary)
 print(len(df_summary))
-#---------------------------------------------------風のスペクトルを図で見る-------------------------------------------------
+#---------------------------------------------------風のパワースペクトルを図で見る-------------------------------------------------
 plt.figure()
 
 year = 2022
@@ -157,12 +157,11 @@ for i in range(len(df_summary)):
         plt.plot(freq, eval(f"spectrum_{block_no}_{year}"), color='green', label=format(wind, '.1f')+"m/s")
     
     slope_all = df_summary.loc[i, "slope_all"]
-    # slope_high = df_summary.loc[i, "slope_high"]
-    # slope_low = df_summary.loc[i, "slope_low"]
+    slope_high = df_summary.loc[i, "slope_high"]
+    slope_low = df_summary.loc[i, "slope_low"]
     plt.plot(selected_freq_all, np.power(10, slope_all*np.log10(selected_freq_all)+eval(f"intercept_{block_no}_all")), color = 'blue')
-    # plt.plot(selected_freq_high, np.power(10, slope_high*np.log10(selected_freq_high)+eval(f"intercept_{block_no}_high")), color = 'blue')
-    # plt.plot(selected_freq_low, np.power(10, slope_low*np.log10(selected_freq_low)+eval(f"intercept_{block_no}_low")), color = 'blue')
-    # plt.plot(selected_freq_all, np.power(10, -2/3*np.log10(selected_freq_all)-3.5), color = 'green')
+    plt.plot(selected_freq_high, np.power(10, slope_high*np.log10(selected_freq_high)+eval(f"intercept_{block_no}_high")), color = 'blue')
+    plt.plot(selected_freq_low, np.power(10, slope_low*np.log10(selected_freq_low)+eval(f"intercept_{block_no}_low")), color = 'blue')
     
 plt.legend(loc="lower right")
 plt.xlabel("f[1/s]", fontsize=14)
@@ -197,23 +196,23 @@ for i in range(len(df_summary)):
         plt.scatter(mean_wind, slope_all, color = "green", label = "3hour~24hour")
     else:
         plt.scatter(mean_wind, slope_all, color = "green")
-# for i in range(len(df_summary)):
-#     mean_wind = df_summary.loc[i, "mean_wind"]
-#     slope_low = df_summary.loc[i, "slope_low"]
-#     if i == 0:
-#         plt.scatter(mean_wind, slope_low, color = "red", label = "3hour~7hour")
-#     else:
-#         plt.scatter(mean_wind, slope_low, color = "red")
+for i in range(len(df_summary)):
+    mean_wind = df_summary.loc[i, "mean_wind"]
+    slope_low = df_summary.loc[i, "slope_low"]
+    if i == 0:
+        plt.scatter(mean_wind, slope_low, color = "red", label = "3hour~7hour")
+    else:
+        plt.scatter(mean_wind, slope_low, color = "red")
         
-#     if slope_low > 0:
-#         print(df_summary.loc[i, "block_no"])
-# for i in range(len(df_summary)):
-#     mean_wind = df_summary.loc[i, "mean_wind"]
-#     slope_high = df_summary.loc[i, "slope_high"]
-#     if i == 0:
-#         plt.scatter(mean_wind, slope_high, color = "cyan", label = "2hour~3.5hour")
-#     else:
-#         plt.scatter(mean_wind, slope_high, color = "cyan")
+    if slope_low > 0:
+        print(df_summary.loc[i, "block_no"])
+for i in range(len(df_summary)):
+    mean_wind = df_summary.loc[i, "mean_wind"]
+    slope_high = df_summary.loc[i, "slope_high"]
+    if i == 0:
+        plt.scatter(mean_wind, slope_high, color = "cyan", label = "2hour~3.5hour")
+    else:
+        plt.scatter(mean_wind, slope_high, color = "cyan")
     
 
 plt.legend(loc="upper right")
